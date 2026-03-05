d031877be0af69be9a9cea8b0466997507c4ab31
Yannick Laret <yannicklaret@Yannicks-MacBook-Pro.local>
2026-03-05 18:40:28 +0100

    infty rho for Escat

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ n2 = np.sqrt(1e8)
 k1 = 2*np.pi/wavelength *n1
 k2 = k1 * n2
 a = wavelength
-rho = 1
+rho = 1e5
 # m should be m>>ka
 m_max = 40  # +1 of what comes out of the tolerance function, otherwise it is not working
 phi = np.linspace(-np.pi, np.pi, m_max*2)
@@ -29,11 +29,13 @@ E0 = 1
 
 ### SUBPROBLEMS
 ## 1
+# compute fields
 am_r, am_t = f.compute_complex_amplitudes(k1, k2, m_max, a)
 Ez, Escat = f.compute_E_z(rho, phi, k1, k2, am_r, am_t, E0, a)
 H_phi = f.compute_H_phi(rho, phi, k1, k2, am_r, am_t, E0, Y1, a)
 sigma = f.compute_sigma(phi, k1, k2, a, m_max, E0)
-sigma_scat = f.compute_sigma_scat(1e5, phi, Escat, E0) # sigma scat geeft een gekke grafiek
+sigma_scat = f.compute_sigma_scat(rho, phi, Escat, E0)
+# plots
 f.plot_am_log(am_r, m_max)
 f.plot_sigma(phi, sigma, wavelength)
 f.plot_sigma(phi, sigma_scat, wavelength)
