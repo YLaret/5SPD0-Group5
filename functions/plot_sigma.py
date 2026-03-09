fde83fc5be019839654c8a1fd9379cc6c38e738c
Yannick Laret <yannicklaret@Yannicks-MacBook-Pro.local>
2026-03-09 21:09:28 +0100

    deg instead of rad and axis limit

--- functions/plot_sigma.py
+++ functions/plot_sigma.py
@@ -4,8 +4,9 @@ import numpy as np
 def plot_sigma(phi, sigma, wavelength):
     sig_lab = 10*np.log10(sigma/wavelength)
     plt.figure()
-    plt.plot(phi, sig_lab)
-    plt.xlabel(r'$\phi$ [rad]')
+    plt.plot(phi/2/np.pi*360, sig_lab)
+    plt.xlim(0,180)
+    plt.xlabel(r'$\phi$ [deg]')
     plt.ylabel(r'$\sigma/\lambda$ [dB]')
     plt.title(r'$\sigma/\lambda$ as a function of $\phi$')
     plt.grid()
