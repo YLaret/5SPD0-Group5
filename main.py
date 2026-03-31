11678b05801dc46dcb7407293b5518f75aa19c7f
NikaVredenbregt <76155860+NikaVredenbregt@users.noreply.github.com>
2026-03-31 14:24:59 +0200

    update

--- main.py
+++ main.py
@@ -33,64 +33,90 @@ Y1 = k1/(omega*mu1)
 Y2 = np.sqrt(1/mu1)  # epsilon 1 = 1
 Z1 = 1/Y1
 
-# ### SUBPROBLEMS
-# ## 1
-# # set homogeneous PEC interior like NASA paper:
+### SUBPROBLEMS
+## 1
+# set homogeneous PEC interior like NASA paper
 n1 = np.sqrt(1)
 n2 = np.sqrt(1e8)
 k1 = 2*np.pi/wavelength *n1
 k2 = k1 * n2
 
-
-# # compute fields
+# Compute fields
 am_r, am_t = f.compute_complex_amplitudes(k1, k2, m_max, a)
 Ez, Escat = f.compute_E_z(rho, phi, k1, k2, am_r, am_t, E0, a)
 H_phi = f.compute_H_phi(rho, phi, k1, k2, am_r, am_t, E0, Y1, a)
 sigma_scat = f.compute_sigma_scat(rho, phi, Escat, E0)   # functie uit reader
 
-# plots
+# Plots
 f.plot_am_log(am_r, m_max)
 f.plot_sigma(phi, sigma_scat, wavelength)
 
 ## 2
-rho = 5
+rho = (a + b) / 2
 # ANALYTICAL
-# set PEC coating like NASA paper
+# Set PEC coating like NASA paper
 n1 = np.sqrt(1)
 n2 = np.sqrt(2)
-k1 = 2*np.pi/wavelength *n1
-k2 = k1 * n2
+k1 = omega * np.sqrt(mu0 * n1**2 * eps0)
+k2 = omega * np.sqrt(mu0 * n2**2 * eps0)
 
 Y1 = k1/(omega*mu1)
 Y2 = np.sqrt(1/mu1)
 
-# compute fields
+# Compute fields
 am_r, am_c = f.compute_complex_amplitudes_coated_PEC(k1, k2, m_max, a,b)
 Ez_cP, Escat_cP, Ein_cP = f.compute_E_z_coated_PEC(rho, phi, k1, k2, am_r, am_c, E0, a, b)
 sigma_scat_cP = f.compute_sigma_scat(rho, phi, Escat_cP, E0)   # functie uit reader
 
 # NUMERICAL
 def n_function(r):
-    return n2 # Or any refractive index profile n(r)
+    return n2
 
-# 1. Calculate the hybrid field (Numerical inside, Analytical outside)
+# Calculate the hybrid field (numerical inside, analytical outside)
 Ez_hybrid = f.compute_fields_hybrid(rho, phi, k1, n1, n_function, a, b, m_max, E0, Y1)
 
-# 2. Calculate the purely Analytical field (for comparison)
-# Use your existing compute_E_z_coated_PEC function here
+# Calculate the analytical field for comparison
 Ez_analytical, _, _ = f.compute_E_z_coated_PEC(rho, phi, k1, k2, am_r, am_c, E0, a, b)
 
 # 3. Plot - They should now overlap in absolute value and phase!
+# plt.figure(figsize=(10,6))
+# plt.plot(phi, np.real(Ez_hybrid), label="Hybrid (Num/Analyt)")
+# plt.plot(phi, np.real(Ez_analytical), "--", label="Analytical Reference")
+# plt.title(f"Comparison of Total Real Field at rho={rho}")
+# plt.xlabel("Phi (rad)")
+# plt.ylabel("Re(Ez)")
+# plt.legend()
+# plt.show()
+
+## 3
+rho_3 = 6*wavelength
+def n_function(r, n1, n2, rho_3):
+    epsr = (n2**2 - n1**2) * (1 - rho_3**2 / a**2) + n1**2
+    return epsr
+
+Ez_hybrid_3 = f.compute_fields_graded_index(rho_3, phi, k1, n1, n2, n_function, a, b, m_max, E0, Y1)
+
+# Plot for comparison
 plt.figure(figsize=(10,6))
 plt.plot(phi, np.real(Ez_hybrid), label="Hybrid (Num/Analyt)")
 plt.plot(phi, np.real(Ez_analytical), "--", label="Analytical Reference")
-plt.title(f"Comparison of Total Real Field at rho={rho}")
+plt.plot(phi, np.real(Ez_hybrid_3), ".", label="Graded index")
+
+plt.title(f"Comparison of Total Real Field at rho={rho_3}")
 plt.xlabel("Phi (rad)")
 plt.ylabel("Re(Ez)")
 plt.legend()
 plt.show()
 
-
+# |E|^2/E0 plot
+Ez_plot = abs(Ez_hybrid_3)**2 / E0
+plt.figure(figsize=(10,6))
+plt.plot(phi, np.real(Ez_plot))
+plt.title(f"Ez for graded-index cylinder at rho={rho_3}")
+plt.xlabel("Phi (rad)")
+plt.ylabel("Re(Ez)")
+plt.grid()
+plt.show()
 
 # # compute integrate fields
 # epsr = np.sqrt(2)
